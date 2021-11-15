de601f0dc06c5e212a12bec655fa934d17d8f0de
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2021-11-15 13:22:42 +0900

    초기 커밋.

---
+++ ObjectOriented05_3/overloading.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <string>
+class Animal {
+private:
+	std::string name;
+public:
+	Animal() {};
+	Animal(std::string name) : name(name) {};
+	void showName() {
+		std::cout << "Name is " << name << std::endl;
+	}
+	//operator+ 추가하세요
+	Animal operator + (const Animal& a) {
+		return name + a.name;
+	}
+};
+int main() {
+	Animal cat("Nabi");
+	cat.showName();
+	Animal dog("Jindo");
+	dog.showName();
+	Animal catDog = dog + cat; //개냥이 합성
+	catDog.showName();
+	dog.showName();
+	getchar();
+	return 0;
+}
