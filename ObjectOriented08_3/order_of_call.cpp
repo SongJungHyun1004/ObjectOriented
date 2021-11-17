96288e521fd48063c80521bf7155c139e6ee0118
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2021-11-17 10:08:09 +0900

    초기 커밋.

---
+++ ObjectOriented08_3/order_of_call.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <string>
+#include <iostream>
+class Base1 {
+public:
+	Base1(int n) : vector_(n, 3) { std::cout << "1" << std::endl; }
+	//.....
+private:
+	std::vector<char> vector_;
+};
+class Base2 {
+public:
+	Base2(int n) : vector_(n, 3) { std::cout << "2" << std::endl; }
+	//.....
+private:
+	std::vector<char> vector_;
+};
+class Derived : public Base2, Base1 {
+public:
+	Derived(const std::string& str) : Base2(1024), Base1(512)
+	{
+		i = 0;
+		std::cout << "3" << std::endl;
+	}
+	// ...
+private:
+	std::string str_;
+	int i = 0;
+};
+int main() {
+	Derived drv("1");
+	return 0;
+}
